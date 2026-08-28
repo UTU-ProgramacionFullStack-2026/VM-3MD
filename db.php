848bcfa5ba6e4489540d17b882df47b706c263e0
gerardor <gerardor.com@gmail.com>
2026-08-28 14:38:40 -0300

    primer commit

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$db = 'hospital_documentos_practica';
+$usuario = 'root';
+$contrasena = '';
+
+$conn = new mysqli($host, $usuario, $contrasena, $db);
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('db.php');
+$sql = "SELECT * FROM usuario";
+$resultado = $conn->execute_query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de usuarios</title>
+</head>
+
+<body>
+    <button>Nuevo usuario</button>
+    <table>
+        <thead>
+            <tr>
+                <td>Nombre</td>
+                <td>Mail</td>
+                <td>Rol</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($resultado as $fila): ?>
+                <tr>
+                    <!-- <td><?php echo $fila['usuario_id']; ?></td> -->
+                    <td><?= $fila['nombre']; ?></td>
+                    <td><?= $fila['email']; ?></td>
+                    <td><?= $fila['rol']; ?></td>
+                    <td><a href="#">Editar</a> <a href="#">Borrar</a></td>
+                </tr>
+
+            <?php endforeach ?>
+
+
+        </tbody>
+
+    </table>
+
+</body>
+
+</html>
